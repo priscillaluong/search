097ad2c82cef844e7a9bea725ec980ee91480843
Priscille <p-luong@live.co.uk>
2020-05-24 17:43:40 +0100

    Create search.py

---
+++ search.py
@@ -0,0 +1,9 @@
+# In this project, I will create a program to search for recipes based on an ingredient. 
+# The project uses the Food2Fork API. 
+# 
+# 1. Ask the user to enter an ingredient they want to search for 
+# 2. Create a function that makes a request to the Food2Fork API 
+# with the required ingredient as part of the search query. 
+# 3. Get the returned recipes from the API response 
+# 4. Display the recipes for each search result.
+
