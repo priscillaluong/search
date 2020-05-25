62d7a4cbb00c0dbc7dea2b687bdae75d2a3211d3
Priscille <p-luong@live.co.uk>
2020-05-25 13:34:57 +0100

    Update spreadsheet_analysis.py

--- spreadsheet_analysis.py
+++ spreadsheet_analysis.py
@@ -5,3 +5,10 @@
 # 2. Collect all of the sales from each month into a single list
 # 3. Output the total sales across all months
 
+import csv
+
+with open('sales.csv', 'r') as sales:
+    spreadsheet = csv.DictReader(sales)
+    for column in spreadsheet:
+        list_of_sales = column['sales']
+        print(list_of_sales)
